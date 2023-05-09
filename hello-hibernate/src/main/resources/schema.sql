fc726d11d39b75faeca35caefdde4e50b1602b6b
adrianpusty <adrianpusty@protonmail.com>
2023-05-09 21:23:15 +0200

    schema updated according to join table

--- hello-hibernate/src/main/resources/schema.sql
+++ hello-hibernate/src/main/resources/schema.sql
@@ -2,4 +2,4 @@ insert into A (id) values (101);
 insert into A (id) values (102);
 insert into B (id) values (1);
 insert into B (id) values (2);
-insert into A_B (AS_ID, BS_ID) values (102, 1);
+insert into A_B (A_ID, b_id) values (102, 1);
